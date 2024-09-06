d73d07d7a564b7c0c9b64b7b188351f06ae59fe6
emanuel.trandafir <emanuel.trandafir@betvictor.com>
2024-09-06 16:44:17 +0200

    using mysql db

---
+++ src/test/java/com/etr/demo/ModelBasedTest.java
@@ -0,0 +1,50 @@
+package com.etr.demo;
+
+import com.etr.demo.MbtJqwikActions.TestedVsModel;
+import com.etr.demo.utils.TestHttpClient;
+import net.jqwik.api.*;
+import net.jqwik.api.stateful.ActionSequence;
+import net.jqwik.testcontainers.Container;
+import net.jqwik.testcontainers.Testcontainers;
+import org.testcontainers.containers.DockerComposeContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+
+import java.io.File;
+
+@Testcontainers
+class ModelBasedTest {
+
+	@Container
+	static DockerComposeContainer<?> ENV = new DockerComposeContainer<>(
+				new File("src/test/resources/docker-compose-test.yml"))
+			.withExposedService("app-tested", 8080,
+					Wait.forHttp("/api/employees").forStatusCode(200))
+			.withExposedService("app-model", 8080,
+					Wait.forHttp("/api/employees").forStatusCode(200));
+
+	@Property(tries = 110)
+	void regressionTest(@ForAll("mbtActions") ActionSequence<TestedVsModel> actions) {
+		TestedVsModel testVsModel = new TestedVsModel(
+				testClient("app-tested"),
+				testClient("app-model"));
+
+		actions.run(testVsModel);
+	}
+
+	private TestHttpClient testClient(String service) {
+		int port = ENV.getServicePort(service, 8080);
+		return new TestHttpClient(service, "http://localhost:%s/api/employees".formatted(port));
+	}
+
+	@Provide
+	Arbitrary<ActionSequence<TestedVsModel>> mbtJqwikActions() {
+		return Arbitraries.sequences(
+				Arbitraries.oneOf(
+						MbtJqwikActions.getOneEmployeeAction(),
+						MbtJqwikActions.getEmployeesByDepartmentAction(),
+						MbtJqwikActions.createEmployeeAction(),
+						MbtJqwikActions.updateEmployeeNameAction()));
+	}
+
+}
+
